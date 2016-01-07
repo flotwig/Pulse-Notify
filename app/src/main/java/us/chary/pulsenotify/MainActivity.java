4f1f838bc8af8af1e83ac206f91a81c6c841a7c1
Caleb Lewis <caleb_dre@yahoo.com>
2016-01-06 16:03:35 -0800

    added a way to test

--- app/src/main/java/us/chary/pulsenotify/MainActivity.java
+++ app/src/main/java/us/chary/pulsenotify/MainActivity.java
@@ -1,13 +1,11 @@
 package us.chary.pulsenotify;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +16,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.thing).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                
             }
         });
     }
